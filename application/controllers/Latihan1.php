63cd777eb3438ded480305fb6de3ad60258409e8
AnindyaHassya <hassya59@gmail.com>
2023-11-30 13:31:27 +0700

    P2 latihan 1

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -3,10 +3,10 @@ class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Selamat Datang.. selamat belajar Web Programming";
+        echo "Selamat Datang, selamat belajar Web Programming";
         //$this->load->view('view-latihan1');
     }
-
+//Latihan1.php//
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('Model_latihan1');
