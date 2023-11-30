b3d856fcc8efe09dd38c127c3ac325dcc2761919
AnindyaHassya <hassya59@gmail.com>
2023-11-30 14:11:49 +0700

    P4 v_about.php

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,24 +2,24 @@
 	<h1><?php echo $judul ?></h1>
 	<h4>Nama</h4>
 	<ul type="disc">
-		<li>Nama Depan : Imam</li>
-		<li>Nama Belakang : Nawawi</li>
+		<li>Nama Depan : Anindya </li>
+		<li>Nama Belakang : Layalia</li>
 	</ul>
 	<br>
 	<h4>Alamat</h4>
 	<ul type="none">
-		<li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+		<li> Bekasi</li>
 	</ul>
 	
 	<h4>Tempat Lahir</h4>
 	<ul type="none">
-		<li>Cirebon</li>
+		<li>Jakarta</li>
 	</ul>
 
 	<h4>Olah Raga Favorit</h4>
 	<ul type="square">
 		<li>Bulutangkis</li>
-		<li>Catur</li>
+		<li>Lari</li>
 	</ul>
 	
 
