34840844698ed06971dea872925c5aa4b171ac88
AnindyaHassya <hassya59@gmail.com>
2023-12-02 22:30:18 +0700

    P9 Membuat halaman admin

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-book"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Pustaka Booking</div>
+                <div class="sidebar-brand-text mx-3">Pustaka booking</div>
             </a>
 
             <!-- Divider -->
