e6ebacebcf8bca65d6231325820032d2f84b955f
AnindyaHassya <hassya59@gmail.com>
2023-12-02 22:56:22 +0700

    P10 Membuat helper

--- application/views/autentifikasi/gagal.php
+++ application/views/autentifikasi/gagal.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Gagal Aktifasi Akun </title>
+    <title>Gagal aktifasi akun </title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
