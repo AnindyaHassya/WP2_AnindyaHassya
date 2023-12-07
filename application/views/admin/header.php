99435fc342146f29a27035cbd2bfdc3e8490a6d5
AnindyaHassya <hassya59@gmail.com>
2023-12-07 19:22:07 +0700

    P12 Header

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Dashboard - Aplikasi perpustakaan</title>
+	<title>Dashboard - Aplikasi Perpustakaan</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.css' ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/datatable/datatables.css' ?>">
 	<script type="text/javascript" src="<?php echo base_url().'assets/js/jquery.js'; ?>"></script>
